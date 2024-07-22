b9bc5418d8b92620484800f4442aaa67245cc4aa
Sanjay <sanjabh11@gmail.com>
2024-07-22 15:48:42 +0530

    Add basic React app structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Quanta Meditation</h1>
+        <p>Explore the intersection of Quantum Physics and Meditation.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
