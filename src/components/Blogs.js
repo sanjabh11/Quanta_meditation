ed711b63f7f47673dbf0dba63ae8535cea11b126
sanjabh11 <33122397+sanjabh11@users.noreply.github.com>
2024-07-31 12:53:45 +0530

    Update Blogs.js

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function Blogs() {
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <p>Welcome to the Blogs section. Here you can find articles related to meditation and quantum physics.</p>
+    </div>
+  );
+}
+
+export default Blogs;
 
