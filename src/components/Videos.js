1a6a41f3a6acb2e465856f372eb907a892cd15ec
sanjabh11 <33122397+sanjabh11@users.noreply.github.com>
2024-07-31 12:54:06 +0530

    Update Videos.js

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function Videos() {
+  return (
+    <div>
+      <h2>Videos</h2>
+      <p>Welcome to the Videos section. Here you can find videos related to meditation and quantum physics.</p>
+    </div>
+  );
+}
+
+export default Videos;
 
